4c636e01dbcbec0ca9eae2d5b08e3b5ec16596d7
Michael <M_Cole84@hotmail.com>
2019-08-15 17:59:00 -0400

    completed adding Sign in/ out

--- WishList/Models/AccountViewModels/LoginViewModel.cs
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -6,21 +6,16 @@ using System.Threading.Tasks;
 
 namespace WishList.Models.AccountViewModels
 {
-    public class RegisterViewModel
+    public class LoginViewModel
     {
         [Required]
-        [DataType(DataType.EmailAddress)]
         [EmailAddress]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(maximumLength:100,MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Compare("Password")]
-        public string ConfirmPassword { get; set; }
     }
 }
